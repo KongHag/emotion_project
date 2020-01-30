6df3902e7b25cf810bdf71095c46508a9628d645
KongHag <timotheehaguenauer@yahoo.fr>
2020-01-30 11:54:16 +0100

    add random batch generator

--- read_data.py
+++ read_data.py
@@ -160,8 +160,9 @@ def all_features(movie_id):
 
 def get_window(movie_id, seq_len, start):
     T = all_features(movie_id)
+    VA_T = valence_arousal(movie_id)
     starting_index = start*(T.shape[0]-seq_len+1)
-    return T[starting_index:starting_index + seq_len,:]
+    return T[starting_index:starting_index + seq_len,:], VA_T[starting_index:starting_index + seq_len,:]
 
 
 if __name__ == '__main__':
