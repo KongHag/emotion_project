cb004818f0dd9051c7cf000ea37dd2adb1931c63
augustin-ardon <augustin.ardon@gmail.com>
2020-03-12 11:35:35 +0100

    fc net

--- model.py
+++ model.py
@@ -45,3 +45,19 @@ class RecurrentNet(nn.Module):
         cell = Variable(torch.zeros(self.num_hid, batch_size, self.hid_dim))
 
         return hidden, cell
+
+import torch.nn.functional as F
+
+class FCNet(nn.Module):
+
+    def __init__(self):
+        super(FCNet, self).__init__()
+        self.fc1 = nn.Linear(5367, 1000)
+        self.fc2 = nn.Linear(1000, 1000)
+        self.fc3 = nn.Linear(1000, 2)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
