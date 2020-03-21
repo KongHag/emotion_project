f962ace9f0789fec6a3ee970ca55487acf406a38
quenting44 <45557362+quenting44@users.noreply.github.com>
2020-03-21 16:47:43 +0100

    Update schedule_train.py

--- schedule_train.py
+++ schedule_train.py
@@ -1,11 +1,12 @@
 from emotion import run, save_config_and_results
 import logging
 import sys
+from dataset import MediaEval18
 
 config = {
-    'seq_len': 20,
+    'seq_len': 64,
     'num_hidden': 2,
-    'hidden_size': 512,
+    'hidden_size': 128,
     'lr': 0.001,
     'batch_size': 16,
     'grad_clip': 1,
@@ -16,22 +17,27 @@ config = {
     'bidirect': True,
     'dropout': 0.3,
     'logger_level': 20,
-    'fragment': 1
+    'fragment': 0.01,
+    'features': 'visual'
 }
 
 # Parse args
 logger = logging.getLogger()
 logger.setLevel(config['logger_level'])
 
+features = MediaEval18._features_len.keys()
 
-for arg_name, arg in config.items():
-    logger.info(
-        "initialization -- {} - {}".format(arg_name, arg))
+for feature in features:
+    config['features']=feature
 
-try:
-    train_losses, test_losses = run(config)
-    save_config_and_results(
-        config, train_losses, test_losses)
+    for arg_name, arg in config.items():
+        logger.info(
+            "initialization -- {} - {}".format(arg_name, arg))
 
-except Exception as exception:
-    logger.critical(sys.exc_info())
+    try:
+        train_losses, test_losses = run(config)
+        save_config_and_results(
+            config, train_losses, test_losses)
+
+    except Exception as exception:
+        logger.critical(sys.exc_info())
