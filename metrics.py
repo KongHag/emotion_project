2990732353cc36255ca4a2a489867002ffe0ff83
Ldormoy6 <33019020+Ldormoy6@users.noreply.github.com>
2020-03-29 18:37:24 +0200

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -41,9 +41,9 @@ def get_metrics(model, testloader):
         flattenX = torch.flatten(X, end_dim=1)
         flattenY = torch.flatten(Y, end_dim=1)
         flattenOutput = torch.flatten(gpu_output.to(device=torch.device('cpu')), end_dim=1)
-        inputs = torch.cat((inputs, flattenX), 2)
-        outputs = torch.cat((outputs, flattenOutput), 2)
-        labels = torch.cat((labels, flattenY), 2)
+        inputs = torch.cat((inputs, flattenX), 1)
+        outputs = torch.cat((outputs, flattenOutput), 1)
+        labels = torch.cat((labels, flattenY), 1)
     
     predictions_valence, predictions_arousal = outputs[0][:], outputs[1][:]
     label_valence, label_arousal = labels[0][:], labels[1][:]
